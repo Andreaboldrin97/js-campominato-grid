31b16398fc5a0f5aa2603f0bc9648b1e0307373b
Andreaboldrin97 <boldrinandrea23@gmail.com>
2022-06-23 14:22:50 +0200

    add html , css , js link

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// Consegna
+// L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
+// Ogni cella ha un numero progressivo, da 1 a 100.
+// Ci saranno quindi 10 caselle per ognuna delle 10 righe.
+// Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console 
+// con il numero della cella cliccata.
+
+//*recupero il buttun dal dom e lo assoccio ad una variabile
+let btnPlay = document.getElementById('play_btn');
+
+//*creo addEvenLIsener sul btn
+btnPlay.addEventListener('click',function(){
+
+    //? creo l'elemto da inportare nell'html
+})
